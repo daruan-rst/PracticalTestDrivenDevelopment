0497b50b58f4b76cf251dbdb22e40fa151e7c9ba
daruan <daruan.rst@gmail.com>
2022-07-05 22:06:19 -0300

    Initial commit

---
+++ src/test/java/com/virtualpairprogrammers/isbntools/ValidateISBNTests.java
@@ -0,0 +1,13 @@
+package com.virtualpairprogrammers.isbntools;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class ValidateISBNTests {
+
+    @Test
+    public void exampleTest(){
+        fail("not yet implemented");
+    }
+}
