f125ca11d6dc9c5e6b616ef70bed7b50737d1765
daruan <daruan.rst@gmail.com>
2022-07-06 20:16:33 -0300

    ValidateISBN first tests, Challenges 1 and 2

--- src/test/java/com/virtualpairprogrammers/isbntools/ValidateISBNTests.java
+++ src/test/java/com/virtualpairprogrammers/isbntools/ValidateISBNTests.java
@@ -2,12 +2,35 @@ package com.virtualpairprogrammers.isbntools;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ValidateISBNTests {
 
     @Test
-    public void exampleTest(){
-        fail("not yet implemented");
+    public void checkValidISBN(){
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("0140449116");
+        assertTrue(result, "first value");
+        result = validator.checkISBN("0140177396");
+        assertTrue(result, "second value");
+    }
+
+    @Test
+    public void checkInvalidISBN(){
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("0140449117");
+        assertFalse(result);
+    }
+
+    @Test
+    public void nineDigitsISBNAreNotAllowed(){
+        ValidateISBN validator = new ValidateISBN();
+        assertThrows(NumberFormatException.class, ()->validator.checkISBN("123456789"));
+    }
+
+    @Test
+    public void nonNumericISBNsAreNotAllowed(){
+        ValidateISBN validator = new ValidateISBN();
+        assertThrows(NumberFormatException.class, ()->validator.checkISBN("helloworld"));
     }
 }
