ca667b2bf23f54906307fa1a45563e759b1e1ecc
daruan <daruan.rst@gmail.com>
2022-07-06 22:18:20 -0300

    Validates ISBN with X final digit and 13 digits long ISBN

--- src/test/java/com/virtualpairprogrammers/isbntools/ValidateISBNTests.java
+++ src/test/java/com/virtualpairprogrammers/isbntools/ValidateISBNTests.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ValidateISBNTests {
 
     @Test
-    public void checkValidISBN(){
+    public void checkValid10DigitsISBN(){
         ValidateISBN validator = new ValidateISBN();
         boolean result = validator.checkISBN("0140449116");
         assertTrue(result, "first value");
@@ -16,9 +16,30 @@ public class ValidateISBNTests {
     }
 
     @Test
-    public void checkInvalidISBN(){
+    public void TenDigitsISBNNumbersEndingWithAnXAreValid(){
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("012000030X");
+        assertTrue(result, "first value");
+    }
+
+    @Test
+    public void checkAValid13DigitISBN(){
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("9781548742287");
+        assertTrue(result);
+    }
+
+    @Test
+    public void checkInvalid10DigitsISBN(){
         ValidateISBN validator = new ValidateISBN();
         boolean result = validator.checkISBN("0140449117");
+        assertFalse(result);
+    }
+
+    @Test
+    public void checkInvalid13DigitsISBN(){
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("9781548742288");
         assertFalse(result);
     }
 
